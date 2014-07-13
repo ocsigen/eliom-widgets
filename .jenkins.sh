92751ade1d17727b6699a3763f08e5e2bc4ac409
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-13 03:30:54 +0200

    Update .jenkins.sh

--- .jenkins.sh
+++ .jenkins.sh
@@ -3,6 +3,7 @@ opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
 opam pin add --no-action ojwidgets 'https://github.com/ocsigen/ojwidgets.git#master'
+opam pin add --no-action ojquery 'https://github.com/ocsigen/ojquery.git#master'
 opam install --deps-only eliom-widgets
 opam install --verbose eliom-widgets
 opam remove --verbose eliom-widgets
