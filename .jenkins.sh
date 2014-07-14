08a7ab3e28b5fca0c8e376060fb276c0766cf266
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:14:19 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,3 +1,4 @@
+
 opam pin add --no-action eliom-widgets .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
@@ -6,4 +7,15 @@ opam pin add --no-action ojwidgets 'https://github.com/ocsigen/ojwidgets.git#mas
 opam pin add --no-action ojquery 'https://github.com/ocsigen/ojquery.git#master'
 opam install --deps-only eliom-widgets
 opam install --verbose eliom-widgets
-opam remove --verbose eliom-widgets
+
+do_build_doc () {
+  make doc
+  mkdir -p $(API_DIR)/server $(API_DIR)/client
+  cp -Rf doc/client/wiki/*.wiki $(API_DIR)/client
+  cp -Rf doc/server/wiki/*.wiki $(API_DIR)/server
+  cp -Rf doc/manual-wiki/*.wiki $(MANUAL_SRC_DIR)/
+}
+
+do_remove () {
+  opam remove --verbose eliom-widgets
+}
