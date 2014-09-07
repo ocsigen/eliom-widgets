a30c2692c03cca89fe9dd99d413c25befcfa9d9c
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-09-07 13:18:25 +0200

    jenkins

--- .jenkins.sh
+++ .jenkins.sh
@@ -3,6 +3,7 @@ opam pin add --no-action eliom-widgets .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
 opam pin add --no-action eliom 'https://github.com/ocsigen/eliom.git#master'
+opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git#master'
 opam pin add --no-action ojwidgets 'https://github.com/ocsigen/ojwidgets.git#master'
 opam pin add --no-action ojquery 'https://github.com/ocsigen/ojquery.git#master'
 opam install --deps-only eliom-widgets
